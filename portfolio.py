eed17fd56460a4b0959c45843dbc38cb11b25f64
HatemHaddad <arabicdatamining@gmail.com>
2020-07-10 20:14:34 +0400

    portfolio

--- portfolio.py
+++ portfolio.py
@@ -8,7 +8,10 @@ tickers = ['MSFT','AAPL','TQQQ']
 mydata = pd.DataFrame()
 
 for t in tickers:
-  mydata[t] = pdr.data.get_data_yahoo(t, start='2020-01-01')
+  mydata[t] = pdr.data.get_data_yahoo(t, start='2020-01-01')['Adj Close']
   
 print(mydata)
 
+
+(mydata/mydata.iloc[0]*100).plot(figsize=(12,8))
+plt.show()
