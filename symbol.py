bf75c4dbd6780489eea27b622b7f9bb999dfdff8
Hatem Haddad <hshadad@emirates.net.ae>
2020-07-10 17:30:04 +0400

    Update symbol.py

--- symbol.py
+++ symbol.py
@@ -3,9 +3,16 @@ import numpy as np
 import pandas_datareader as pdr
 import matplotlib.pyplot as pls
 
+"""
+This function reads a symbol and returns the stock from yahoo finance.
+"""
+
 def equity(symbol):
     stock = pdr.data.get_data_yahoo(symbol,start='2020-01-01')
     return stock
 
 spy = equity('PG')
-print(spy.info())
+
+print(spy.info()) # Info about index, columns , ...
+
+print(spy.shape) # Dataframe dimension
