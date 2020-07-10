5013f48661df6a1c79d631f9a1ca10f542f1f9ad
HatemHaddad <arabicdatamining@gmail.com>
2020-07-10 19:41:48 +0400

    return

--- symbol.py
+++ symbol.py
@@ -11,7 +11,7 @@ def equity(symbol):
     stock = pdr.data.get_data_yahoo(symbol,start='2020-01-01')
     return stock
 
-spy = equity('PG')
+spy = equity('USO')
 
 print(spy.info()) # Info about index, columns , ...
 
@@ -21,4 +21,10 @@ spy['Simple Returns'] = (spy['Adj Close']/spy['Adj Close'].shift(1))-1
 print(spy['Simple Returns'])
 
 spy['Simple Returns'].plot(figsize=(12,8))
-plt.show()
+#plt.show()
+
+avg_return_d = spy['Simple Returns'].mean()
+
+avg_return_a = avg_return_d * 250
+
+print('Annual return : ' + str(round(avg_return_a,1)*100) + ' %')
