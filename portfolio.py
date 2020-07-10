e90e7fa8665a078b9cd6aa0624eecc94f4d80896
Hatem Haddad <hshadad@emirates.net.ae>
2020-07-10 19:54:47 +0400

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -3,3 +3,12 @@ import pandas as pd
 import pandas_datareader as pdr
 import matplotlib.pyplot as plt
 
+tickers = ['MSFT','AAPL','TQQQ']
+
+mydata = pd.DataFrame()
+
+for t in tickers:
+  mydata[t] = pdr.data.get_data_yahoo(t, start='2020-01-01')
+  
+print(mydata)
+
