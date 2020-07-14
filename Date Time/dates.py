623b2dda28d3fb36396af76dbe84fda6989046bd
Hatem Haddad <hshadad@emirates.net.ae>
2020-07-14 21:45:46 +0400

    Update dates.py

--- Date Time/dates.py
+++ Date Time/dates.py
@@ -1,3 +1,9 @@
 from datetime import date
 
 birthdate = date(1970,8,3) # The date class from the library datetime takes and integrt numbers as year, month and day seperated by commas.
+
+# date attributes
+
+birthdate.year
+birthdate.month
+birthdate.day
